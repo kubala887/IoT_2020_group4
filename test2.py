0e403b72f8886153399cc72e5a724fb1197148c1
Jakub Kornaś <40810617+kubala887@users.noreply.github.com>
2020-11-05 12:50:40 +0100

    Add files via upload
    Working algorithm with debug measurement each 1 second

--- test2.py
+++ test2.py
@@ -20,82 +20,82 @@ size = 100.0
 
 #Function for measuring distance
 def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+	# set Trigger to HIGH
+	GPIO.output(GPIO_TRIGGER, True)
  
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	GPIO.output(GPIO_TRIGGER, False)
  
-    StartTime = time.time()
-    StopTime = time.time()
+	StartTime = time.time()
+	StopTime = time.time()
  
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
+	# save StartTime
+	while GPIO.input(GPIO_ECHO) == 0:
+		StartTime = time.time()
  
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+	# save time of arrival
+	while GPIO.input(GPIO_ECHO) == 1:
+		StopTime = time.time()
  
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+	# time difference between start and arrival
+	TimeElapsed = StopTime - StartTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	distance = (TimeElapsed * 34300) / 2
  
-    return distance
+	return distance
 
 #Function to categorize the measurement
 def distCheck(newDistance,dist):
-    #expected states
-    if dist-1 < newDistance < dist+1:
-        setDistance = dist
-        
-    if size >= newDistance >= dist+1:
-        setDistance = distValidate(newDistance,dist);
-        
-    if 0 <= newDistance <= dist-1:
-        setDistance = distValidate(newDistance,dist);
-    #edge cases
-    if size + 3 >= newDistance > size:
-        setDistance = size
-        
-    if distance < 0:
-        setDistance = 0
-    
-    return setDistance
-        
+	#expected states
+	if dist-1 < newDistance < dist+1:
+		setDistance = dist
+		
+	if size >= newDistance >= dist+1:
+		setDistance = distValidate(newDistance,dist);
+		
+	if 0 <= newDistance <= dist-1:
+		setDistance = distValidate(newDistance,dist);
+	#edge cases
+	if size + 3 >= newDistance > size:
+		setDistance = size
+		
+	if distance < 0:
+		setDistance = 0
+	
+	return setDistance
+		
 #Function to validate the measurement
 def distValidate(newDistance,dist):
 	a = []
-    for i in range(10):
-        a[i] = round(distance(),1)
+	for i in range(10):
+		a.append(round(distance(),1))
 	
-    meanVal = np.mean(a)
-    stdVal = np.std(a)
-    
-    if newDistance-0.5 <= meanVal <= newDistance+0.5 and stdVal <= 0.5:
-        validatedDistance = newDistance
-    else:
-        validateDistance = dist
+	meanVal = np.mean(a)
+	stdVal = np.std(a)
+	
+	if newDistance-0.5 <= meanVal <= newDistance+0.5 and stdVal <= 0.5:
+		validatedDistance = newDistance
+	else:
+		validatedDistance = dist
 
-    return validatedDistance
+	return validatedDistance
  
 if __name__ == '__main__':
-    try:
-        dist = round(distance(),1)
-        print ("Measured Initial Distance = " + str(dist))
-        time.sleep(0.1)
-        while True:
+	try:
+		dist = round(distance(),1)
+		print ("Measured Initial Distance = " + str(dist))
+		time.sleep(0.1)
+		while True:
 			dist2 = round(distance(),1)
-            tempDistance = distCheck(dist2,dist)
-            if tempDistance != dist:
-                dist = tempDistance
-            print ("New Measured Distance = " + str(dist))    
-            time.sleep(1)
+			tempDistance = distCheck(dist2,dist)
+			if tempDistance != dist:
+				dist = tempDistance
+			print ("New Measured Distance = " + str(dist))    
+			time.sleep(1)
  
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Stopped by user (ctrl+c or different)")
-        GPIO.cleanup()
+		# Reset by pressing CTRL + C
+	except KeyboardInterrupt:
+		print("Stopped by user (ctrl+c or different)")
+		GPIO.cleanup()
