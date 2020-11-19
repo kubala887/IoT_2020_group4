b0e48b915793f8b6f74b54981a7a5d67dd00bf2e
Katarzyna1997 <48634097+Katarzyna1997@users.noreply.github.com>
2020-11-19 01:51:23 +0100

    Update HTTP.py

--- HTTP.py
+++ HTTP.py
@@ -14,7 +14,10 @@ ServerIP = '18.134.93.173'
 Port = '8086'
 Message = 'Waveshare'
 data = 'measures,type="KRAKOW" ID="111",long=19.888,lat=50.058,fill=96.87,track="1" 1434067467000000202'
-cmd='AT+HTTPDATA='+str(len(data))+',3500'
+#data = 'measures,type="ABBA" ID="666",long=45.888,lat=50.058,fill=96.87,track="1" 1400000000000000000'
+data_binary=data.encode()
+cmd='AT+HTTPDATA='+str(len(data))+',2000'
+print(cmd)
 
 def power_on(power_key):
     print('SIM7600X is starting:')
@@ -38,40 +41,54 @@ def power_down(power_key):
     print('Good bye')
     
 def send_at(command,back,timeout):
-	rec_buff = ''
-	ser.write((command+'\r\n').encode())
-	time.sleep(timeout)
-	if ser.inWaiting():
-		time.sleep(0.1 )
-		rec_buff = ser.read(ser.inWaiting())
-	if rec_buff != '':
-		print(rec_buff.decode())
-		return 1
-	else:
-		print(command + ' no responce')
+    rec_buff = ''
+    ser.write((command+'\r\n').encode())
+    time.sleep(timeout)
+    if ser.inWaiting():
+        time.sleep(0.1 )
+        rec_buff = ser.read(ser.inWaiting())
+    if rec_buff != '':
+        print(rec_buff.decode())
+        return 1
+    else:
+        print(command + ' no responce')
 
 
 try:
     power_on(power_key)
         
-    send_at('AT+CSQ','OK',1)
-    send_at('AT+CREG?','OK',1)
-    send_at('AT+CPSI?','OK',1)
+    #send_at('AT+CSQ','OK',1)
+    #send_at('AT+CREG?','OK',1)
+    #send_at('AT+CPSI?','OK',1)
     send_at('AT+CIPSHUT','OK',1)
     send_at('AT+CGATT=1','OK',1)
     send_at('AT+SAPBR=3,1,"CONTYPE","GPRS"','OK',1)
     send_at('AT+SAPBR=3,1,"APN","internet"','OK',1)
-    send_at('AT+CSTT="internet"','OK',1)
-    send_at('AT+CIICR','OK',1)
-    send_at('AT+HTTPINIT','OK',1)
+    send_at('AT+CSTT="internet"','',1)
     time.sleep(3)
+    send_at('AT+SAPBR=1,1','',1)
+    send_at('AT+CIICR','OK',1)
+    
+    #send_at('AT+SAPBR=3,1,"APN","wap.cingular','',1)
+    #send_at('AT+SAPBR=1,1','',1)
+    
+    send_at('AT+HTTPTERM','OK',1)
+    time.sleep(1)
+    send_at('AT+HTTPINIT','OK',1)
+    time.sleep(1)
+    
     send_at('AT+HTTPPARA="CID",1','OK',1)
+    
+
     send_at('AT+HTTPPARA="URL","http://18.134.93.173:8086/write?db=projekt_IoT"','OK',1)
-    send_at('AT+HTTPPARA="CONTENT","data/binary"','OK',1)
-    send_at(cmd,'DOWNLOAD',1) 
+    send_at('AT+HTTPPARA="CONTENT","data-binary"','OK',1)
+    send_at(cmd,'DOWNLOAD',1)
+    time.sleep(1)
     ser.write(data.encode())
-    send_at('AT+HTTPACTION=1','',1)
-    send_at('AT+HTTPHEAD','',1)
+    print('write finished')
+    time.sleep(1)
+    send_at('AT+HTTPACTION=1','',3)
+    send_at('AT+HTTPHEAD','',3)
     send_at('AT+HTTPTERM','OK',1)
     power_down(power_key)
 
@@ -85,3 +102,4 @@ except:
 if ser != None:
         ser.close()
         GPIO.cleanup()
+
