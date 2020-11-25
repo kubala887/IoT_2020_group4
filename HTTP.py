9669535e6f6edbc502e857a8b997773a6b779699
Katarzyna1997 <48634097+Katarzyna1997@users.noreply.github.com>
2020-11-25 18:06:53 +0100

    Update HTTP.py

--- HTTP.py
+++ HTTP.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python
-
+ 
 import RPi.GPIO as GPIO
 import serial
 import time
-
+ 
 ser = serial.Serial('/dev/ttyS0',115200)
 ser.flushInput()
-
+ 
 power_key = 6
 rec_buff = ''
-APN = 'CMNET'
-ServerIP = '18.134.93.173'
-Port = '8086'
-Message = 'Waveshare'
-data = 'measures,type="CITY" ID="111",long=19.888,lat=50.058,fill=96.87,track="1" 1434067467000000202'
+type="CITY"
+ID="111"
+long="19.888"
+lat="50.058"
+fill="96.87"
+track"1"
+timestamp="1400000000000000000"
+data_measures=',type="'+type+'" ID="'+ID+'",long='+long+',lat='+lat+',fill='+fill+',track='+track+' '+timestamp
+data='measures'+data_measures
+#data = 'measures,type="KRAKOW" ID="111",long=19.888,lat=50.058,fill=96.87,track="1" 1434067467000000202'
+data = 'measures,type="CITY" ID="666",long=45.888,lat=50.058,fill=96.87,track="1" 1400000000000000000'
 data_binary=data.encode()
 cmd='AT+HTTPDATA='+str(len(data))+',2000'
 print(cmd)
-
+ 
 def power_on(power_key):
     print('SIM7600X is starting:')
     GPIO.setmode(GPIO.BCM)
@@ -30,7 +36,7 @@ def power_on(power_key):
     time.sleep(2)
     ser.flushInput()
     print('SIM7600X is ready')
-
+ 
 def power_down(power_key):
     print('SIM7600X is loging off:')
     GPIO.output(power_key,GPIO.HIGH)
@@ -38,7 +44,7 @@ def power_down(power_key):
     GPIO.output(power_key,GPIO.LOW)
     time.sleep(2)
     print('Good bye')
-    
+ 
 def send_at(command,back,timeout):
     rec_buff = ''
     ser.write((command+'\r\n').encode())
@@ -51,14 +57,10 @@ def send_at(command,back,timeout):
         return 1
     else:
         print(command + ' no responce')
-
-
+ 
+ 
 try:
     power_on(power_key)
-        
-    #send_at('AT+CSQ','OK',1)
-    #send_at('AT+CREG?','OK',1)
-    #send_at('AT+CPSI?','OK',1)
     send_at('AT+CIPSHUT','OK',1)
     send_at('AT+CGATT=1','OK',1)
     send_at('AT+SAPBR=3,1,"CONTYPE","GPRS"','OK',1)
@@ -67,18 +69,11 @@ try:
     time.sleep(3)
     send_at('AT+SAPBR=1,1','',1)
     send_at('AT+CIICR','OK',1)
-    
-    #send_at('AT+SAPBR=3,1,"APN","wap.cingular','',1)
-    #send_at('AT+SAPBR=1,1','',1)
-    
     send_at('AT+HTTPTERM','OK',1)
     time.sleep(1)
     send_at('AT+HTTPINIT','OK',1)
-    time.sleep(1)
-    
+    time.sleep(1) 
     send_at('AT+HTTPPARA="CID",1','OK',1)
-    
-
     send_at('AT+HTTPPARA="URL","http://18.134.93.173:8086/write?db=projekt_IoT"','OK',1)
     send_at('AT+HTTPPARA="CONTENT","data/binary"','OK',1)
     send_at(cmd,'DOWNLOAD',1)
@@ -90,14 +85,14 @@ try:
     send_at('AT+HTTPHEAD','',3)
     send_at('AT+HTTPTERM','OK',1)
     power_down(power_key)
-
-   
-
+ 
+ 
+ 
 except:
     if ser != None:
         ser.close()
         GPIO.cleanup()
-
+ 
 if ser != None:
         ser.close()
         GPIO.cleanup()
